0fd7163337005a22e2ac3bb124e95560c08e0afe
javierOrtega95 <fjortega@wizzie.io>
2023-03-11 16:23:06 +0100

    feat: rename project

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+	{
+		userName: "javierOrtega95",
+		name: "Javier Ortega Carrasco",
+		isFollowing: true,
+	},
+	{
+		userName: "elonmusk",
+		name: "Elon Musk",
+		isFollowing: false,
+	},
+	{
+		userName: "aliciarebo",
+		name: "Alicia Rebollo",
+		isFollowing: true,
+	},
+];
+
+export function App() {
+	const format = (username) => `@${username}`;
+	return (
+		<section className="App">
+			{users.map(({ userName, name, isFollowing }) => {
+				return (
+					<TwitterFollowCard
+						key={userName}
+						username={userName}
+						formatUserName={format}
+						name={name}
+						initialIsFollowing={isFollowing}
+					/>
+				);
+			})}
+		</section>
+	);
+}
