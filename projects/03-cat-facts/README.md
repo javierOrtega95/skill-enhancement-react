4ff325b285661b8eb366ef044a5f5fb025936f79
javierOrtega95 <fjortega@wizzie.io>
2023-03-29 09:18:53 +0200

    feat(cat-facts): add project

---
+++ projects/03-cat-facts/README.md
@@ -0,0 +1,10 @@
+# TASK
+
+- Fetch a random cat fact from the first API
+- Fetch de first word of the fact
+- Show a cat image with the first word
+
+APIs:
+
+- Facts Random: https://catfact.ninja/fact
+- Imagen random: https://cataas.com/cat/says/hello
---
+++ projects/03-cat-facts/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+
+const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says';
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
+function App() {
+  const [fact, setFact] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+
+  // fetch fact cat
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
+
+  // fetch cat image url from fact
+  useEffect(() => {
+    if (!fact) return;
+
+    fetch(`${CAT_ENDPOINT_IMAGE_URL}/${fact.split(' ', 1)}?json=true`)
+      .then(res => res.json())
+      .then(data => {
+        const { url } = data;
+        setImageUrl(url);
+      });
+  }, [fact]);
+
+  return (
+    <main>
+      <h1>Random cat facts</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={`${CAT_PREFIX_IMAGE_URL}/${imageUrl}`}
+            alt={`Image extracted using the first word of the fact`}
+          />
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default App;
