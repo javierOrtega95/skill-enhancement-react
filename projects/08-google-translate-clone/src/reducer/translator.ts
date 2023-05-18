81d980857b6c19f6513dd5790e79b1cbd115d508
javierOrtega95 <fjortega@wizzie.io>
2023-05-18 23:21:34 +0200

    feat (08-google-translate-clone): add translator reducer

---
+++ projects/08-google-translate-clone/src/reducer/translator.ts
@@ -0,0 +1,67 @@
+import { AUTO_LANGUAGE } from '../constants'
+import { type Action, type State } from '../types'
+
+export function translatorReducer (state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    if (state.fromLanguage === AUTO_LANGUAGE) return state
+
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      loading,
+      result: '',
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    if (state.fromLanguage === action.payload) return state
+
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      fromLanguage: action.payload,
+      result: '',
+      loading
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    if (state.toLanguage === action.payload) return state
+
+    const loading = state.fromText !== ''
+
+    return {
+      ...state,
+      toLanguage: action.payload,
+      result: '',
+      loading
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    const loading = action.payload !== ''
+
+    return {
+      ...state,
+      loading,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
