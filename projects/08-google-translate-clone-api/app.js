6fdcc62db70d8417e9bbe4df84fb28b45c1766b3
javierOrtega95 <fjortega@wizzie.io>
2023-05-24 21:52:50 +0200

    feat: (08-google-translate-clone-api): add content type urlencoded

--- projects/08-google-translate-clone-api/app.js
+++ projects/08-google-translate-clone-api/app.js
@@ -8,28 +8,34 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
 const API_URL = process.env.DEEPL_API_URL
 const API_KEY = process.env.DEEPL_API_KEY
 
 app.post('/api/translate/', async (req, res) => {
-  const text = req.query.text
-  const targetLanguage = req.query.target_lang
+  const { sourceLanguage, targetLanguage, text } = req.body
 
-  const response = await fetch(`${API_URL}?text=${text}&target_lang=${targetLanguage}`, {
+  const commonData = { target_lang: targetLanguage, text }
+
+  const data = sourceLanguage === 'auto' ? commonData : { ...commonData, source_lang: sourceLanguage }
+
+  const response = await fetch(`${API_URL}`, {
     method: 'POST',
     headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
       Authorization: `DeepL-Auth-Key ${API_KEY}`
-    }
+    },
+    body: new URLSearchParams(data)
   })
 
   if (!response.ok) {
-    return 'Error'
+    return res.status(200).send('Error translating')
   }
 
   const result = await response.json()
-  console.log(result)
+
   res.status(200).send(result)
 })
 
