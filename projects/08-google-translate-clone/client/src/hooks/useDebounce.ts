4e888cc592bd54f02fa5696bce3eed0e8c2c1dd5
javierOrtega95 <fjortega@wizzie.io>
2023-05-29 22:53:32 +0200

    refactor (08-google-translate-clone): lint fixes

--- projects/08-google-translate-clone/client/src/hooks/useDebounce.ts
+++ projects/08-google-translate-clone/client/src/hooks/useDebounce.ts
@@ -8,7 +8,7 @@ export function useDebounce<T> (value: T, delay = 500) {
       setDebouncedValue(value)
     }, delay)
 
-    return () => clearTimeout(timer) 
+    return () => { clearTimeout(timer) }
   }, [value, delay])
 
   return debouncedValue
