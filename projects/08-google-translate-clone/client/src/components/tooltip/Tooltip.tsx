76b6a27512d034b3a8a905043dd20d94912402a2
javierOrtega95 <fjortega@wizzie.io>
2023-06-22 23:58:06 +0200

    styles: set default tooltip position

--- projects/08-google-translate-clone/client/src/components/tooltip/Tooltip.tsx
+++ projects/08-google-translate-clone/client/src/components/tooltip/Tooltip.tsx
@@ -5,13 +5,12 @@ interface Props {
   position?: string
 }
 
-export const Tooltip = ({ text, children, position = 'top' }: Props) => {
+export const Tooltip = ({ text, children, position = 'bottom' }: Props) => {
   return (
     <div className='tooltip'>
       {children}
       <div className={`tooltiptext tooltip-${position}`}>
         {text}
-        <span className='arrow' />
       </div>
     </div>
   )
