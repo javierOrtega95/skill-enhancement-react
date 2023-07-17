1d3b31ac0736bc3659c0c5b5633cbe9c07a23657
javierOrtega95 <fjortega@wizzie.io>
2023-07-17 19:29:27 +0200

    feat (08-google-translate-clone): reusable tooltip component

--- projects/08-google-translate-clone/client/src/components/tooltip/Tooltip.tsx
+++ projects/08-google-translate-clone/client/src/components/tooltip/Tooltip.tsx
@@ -1,17 +1,42 @@
+import { useState } from 'react'
 import './tooltip.css'
+
 interface Props {
   text: string
   children: React.ReactNode
+  delay?: number
   position?: string
 }
 
-export const Tooltip = ({ text, children, position = 'bottom' }: Props) => {
+const Tooltip = ({ text, children, delay, position = 'bottom' }: Props) => {
+  const [active, setActive] = useState(false)
+  let timeout: number
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true)
+    }, delay ?? 300)
+  }
+
+  const hideTip = () => {
+    clearInterval(timeout)
+    setActive(false)
+  }
+
   return (
-    <div className='tooltip'>
+    <div
+      className='tooltip-wrapper'
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
       {children}
-      <div className={`tooltiptext tooltip-${position}`}>
-        {text}
-      </div>
+      {active && (
+        <div className={`tooltip-text ${position}`}>
+          {text}
+        </div>
+      )}
     </div>
   )
 }
+
+export default Tooltip
