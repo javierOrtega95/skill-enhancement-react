f5fc5b0dfde88a4e55e4dba8fe969adbbe371799
javierOrtega95 <fjortega@wizzie.io>
2023-08-04 14:58:59 +0200

    feat (08-google-translate-clone-api): disable cors

--- projects/08-google-translate-clone/api/app.js
+++ projects/08-google-translate-clone/api/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
