8146eebaa8305e65a88f8eaf5fafb3c92e69a3aa
javierOrtega95 <fjortega@wizzie.io>
2023-08-09 13:52:51 +0200

    feat (09-crud-redux-toolkit): add middleware to persist the state in localStorage

--- projects/09-crud-redux-toolkit/src/store/index.ts
+++ projects/09-crud-redux-toolkit/src/store/index.ts
@@ -1,10 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
+
+const persistanceLocalStorageMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    // before updating the state
+    next(action);
+    // after updating the state
+    localStorage.setItem("__redux__state__", JSON.stringify(store.getState()));
+  };
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
+  middleware: [persistanceLocalStorageMiddleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
