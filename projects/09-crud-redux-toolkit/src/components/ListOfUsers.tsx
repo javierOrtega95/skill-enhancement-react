5a8d3f06278e81ea918a86af306d83ccc9d2a89b
javierOrtega95 <fjortega@wizzie.io>
2023-08-10 19:26:02 +0200

    feat (09-crud-redux-toolkit): add modal to create or edit a user

--- projects/09-crud-redux-toolkit/src/components/ListOfUsers.tsx
+++ projects/09-crud-redux-toolkit/src/components/ListOfUsers.tsx
@@ -1,5 +1,6 @@
 import {
   Badge,
+  Button,
   Card,
   Flex,
   Table,
@@ -10,88 +11,133 @@ import {
   TableRow,
   Title,
 } from "@tremor/react";
+import { useRef, useState } from "react";
 import { useAppSelector } from "../hooks/store";
 import { useUserActions } from "../hooks/useUserActions";
+import { UserWithId } from "../store/users/slice";
+import { UserModal } from "./UserModal";
 
 export default function ListOfUsers() {
   const users = useAppSelector((state) => state.users);
   const { removeUser } = useUserActions();
 
-  return (
-    <Card className="mt-3">
-      <Flex justifyContent="start" className="space-x-2">
-        <Title>Users</Title>
-        <Badge>{users.length}</Badge>
-      </Flex>
-      <Table className="mt-2">
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>Id</TableHeaderCell>
-            <TableHeaderCell>Name</TableHeaderCell>
-            <TableHeaderCell>Email</TableHeaderCell>
-            <TableHeaderCell>Actions</TableHeaderCell>
-          </TableRow>
-        </TableHead>
+  const [openUserModal, setOpenUserModal] = useState(false);
+  const userRef = useRef<UserWithId>();
 
-        <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.name}>
-              <TableCell>{user.id}</TableCell>
-              <TableCell style={{ display: "flex", alignItems: "center" }}>
-                <img
-                  style={{
-                    width: "32px",
-                    height: "32px",
-                    borderRadius: "50%",
-                    marginRight: "8px",
-                  }}
-                  src={`https://unavatar.io/github/${user.github}`}
-                  alt={user.name}
-                />
-                {user.name}
-              </TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell>
-                <button type="button">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <title>Edit element</title>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                    />
-                  </svg>
-                </button>
-                <button onClick={() => removeUser(user.id)} type="button">
-                  <svg
-                    aria-label="Remove element"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <title>Remove element</title>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </button>
-              </TableCell>
+  const handleOpenUserModal = (user?: UserWithId) => {
+    userRef.current = user;
+    setOpenUserModal(true);
+  };
+
+  const handleCloseUserModal = () => {
+    userRef.current = undefined;
+    setOpenUserModal(false);
+  };
+
+  return (
+    <>
+      <Card className="mt-3">
+        <Flex justifyContent="between" className="space-x-2">
+          <Flex justifyContent="start">
+            <Title>Users</Title>
+            <Badge className="ml-3">{users.length}</Badge>
+          </Flex>
+          <Button variant="light" onClick={() => handleOpenUserModal()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <title>Add user</title>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </Button>
+        </Flex>
+        <Table className="mt-2">
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Id</TableHeaderCell>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell>Email</TableHeaderCell>
+              <TableHeaderCell>Actions</TableHeaderCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Card>
+          </TableHead>
+
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.name}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell style={{ display: "flex", alignItems: "center" }}>
+                  <img
+                    style={{
+                      width: "32px",
+                      height: "32px",
+                      borderRadius: "50%",
+                      marginRight: "8px",
+                    }}
+                    src={`https://unavatar.io/github/${user.github}`}
+                    alt={user.name}
+                  />
+                  {user.name}
+                </TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  <button
+                    type="button"
+                    onClick={() => handleOpenUserModal(user)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <title>Edit element</title>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                  </button>
+                  <button onClick={() => removeUser(user.id)} type="button">
+                    <svg
+                      aria-label="Remove element"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <title>Remove element</title>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+      <UserModal
+        initialOpen={openUserModal}
+        user={userRef.current}
+        onClose={handleCloseUserModal}
+      />
+    </>
   );
 }
