36448b4dae4ce584fcc113de6f4ceb0f5f7434de
javierOrtega95 <fjortega@wizzie.io>
2023-12-31 00:56:31 +0100

    refactor: linter fixes

--- projects/09-crud-redux-toolkit/src/users/components/ListOfUsers.tsx
+++ projects/09-crud-redux-toolkit/src/users/components/ListOfUsers.tsx
@@ -156,7 +156,7 @@ export default function ListOfUsers() {
         user={userRef.current}
         onClose={handleCloseUserForm}
       />
-      
+
       <DeleteUser
         open={openDeleteUser}
         userId={userId.current}
--- projects/09-crud-redux-toolkit/src/users/hooks/useUserActions.ts
+++ projects/09-crud-redux-toolkit/src/users/hooks/useUserActions.ts
@@ -1,9 +1,5 @@
 import { useAppDispatch } from "../../store/hooks/store";
-import {
-  addNewUser,
-  deleteUserById,
-  updateUser,
-} from "../store/slice";
+import { addNewUser, deleteUserById, updateUser } from "../store/slice";
 import { type UserId, type UserWithId } from "../types";
 
 export const useUserActions = () => {
