b75af8b053eceab5871a96bab9ee6b6d2d95fc88
javierOrtega95 <fjortega@wizzie.io>
2024-01-01 16:12:29 +0100

    chore: add Sonner dependency

--- projects/09-crud-redux-toolkit/src/App.tsx
+++ projects/09-crud-redux-toolkit/src/App.tsx
@@ -1,12 +1,16 @@
 import { Title } from "@tremor/react";
+import { Toaster } from "sonner";
 import "./App.css";
 import ListOfUsers from "./users/components/ListOfUsers";
 
 function App() {
   return (
     <>
-      <Title>CRUD React Redux Toolkit + Rome Tools + Tremor</Title>
+      <Title className="text-center py-4">
+        CRUD React Redux Toolkit + Rome Tools + Tremor
+      </Title>
       <ListOfUsers />
+      <Toaster richColors />
     </>
   );
 }
